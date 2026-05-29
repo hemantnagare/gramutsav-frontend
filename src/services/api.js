065c0ae57e70c42d88838cdf4cb1f9fc915f1408
Hemant Nagare <hemantnagare10@gmail.com>
2026-05-29 15:10:30 +0530

    baseURL: "https://gramutsav.onrender.com/api/auth"  updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080/api/auth"
+    baseURL: "https://gramutsav.onrender.com/api/auth"
 });
 
 export default API;
